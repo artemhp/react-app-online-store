67e82ffd53160d84e1f2492cdd8c1b50c353b96d
Artem Deikun <artem.deikun@zorallabs.com>
2022-10-28 13:42:01 -0400

    Integrate React Redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./app/store";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -35,7 +37,11 @@ const router = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
